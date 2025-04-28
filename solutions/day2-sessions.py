b831d3a7b033479bd77eac80201ef5233eabcdda
Randy <randy.au@gmail.com>
2025-04-27 20:45:27 -0400

    remove dateparser dependency

--- solutions/day2-sessions.py
+++ solutions/day2-sessions.py
@@ -1,13 +1,14 @@
 import csv
 import datetime
-import dateparser 
+#import dateparser 
 
 my_file = csv.reader(open('gardening_blog_log.csv','r'))
 sessions = {}
 output = []
 output.append(["timestamp","user_id","page_url","session_id"])
 for timestamp, user, url in my_file:
-    ts = dateparser.parse(timestamp)
+#    ts = dateparser.parse(timestamp)
+    ts  = datetime.fromisoformat(datetime_string)
     user_sessions = sessions.setdefault(user,[ {'session_num':1,'last_evt':ts} ,])
     if user_sessions[-1]['last_evt'] != None \
             and abs((ts - user_sessions[-1]['last_evt']).total_seconds()) > 60*30: #30min session length
